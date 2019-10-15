57a0ce7e6a837cec293abe8cec631f5db552b798
robertoenricon <33502237+robertoenricon@users.noreply.github.com>
2019-10-14 22:09:16 -0300

    PHP Mailer
    PHP Mailer funcionando

--- phpMailer/mail.php
+++ phpMailer/mail.php
@@ -1,35 +1,35 @@
 <?php
-session_start();
-//header('Access-Control-Allow-Origin: *');                     
-date_default_timezone_set("America/Sao_Paulo");
-
 
 if (isset($_POST['mail'])) {
 	
 	$email = $_POST['mail'];
-	print $email;
+	print $email."<br>";
 	
 	require 'PHPMailerAutoload.php';
-	require 'index.php';
 
 	$mail = new PHPMailer;                              
 
 	$mail->CharSet = 'UTF-8';
 	$mail->IsSMTP();
-	// $mail->Host = 'smtp.gmail.com';                 
-	// $mail->Host = 'smtp.live.com';                 
-	// $mail->Host = 'smtp.office365.com';
-	$mail->Host = '10.0.0.135';
-	$mail->SMTPAuth = false;                            
-	// $mail->Username = 'robertoenricon@gmail.com';             
+	
+	// $mail->Host = 'smtp.gmail.com'; //gmail                                
+	$mail->Host = 'smtp.office365.com'; //Hotmail
+	// $mail->Host = '10.0.0.135';
+
+	//Usar autenticação SMTP
+	$mail->SMTPAuth = true;                            
+	// $mail->SMTPAuth = false;                           
+                      
+	// $mail->Username = 'robertoenrico_@hotmail.com';                                
 	$mail->Username = 'roberto.nicolo@integralmedica.com.br';              
-	$mail->Password = $pass;                       
+	$mail->Password = '@!_15385waWA';                       
 	
-	$mail->SMTPSecure = 'STARTTLS';                          
-	$mail->Port = 25;
-	// $mail->Port = 587;
-	
+	// $mail->SMTPSecure = 'STARTTLS'; //office365                         
+	$mail->SMTPSecure = 'tls'; //hotmail                         
 	// $mail->SMTPSecure = 'ssl'; //google                          
+
+	// $mail->Port = 25;
+	$mail->Port = 587; //hotmail
 	// $mail->Port = 465; //google
 	
 	$mail->SMTPDebug = 0; //Desativa a depuração, sendo a opção padrão.
@@ -37,21 +37,71 @@ if (isset($_POST['mail'])) {
 	// $mail->SMTPDebug = 2; //Exibe mensagens do cliente e servidor
 	// $mail->SMTPDebug = 4; //Exibe todas as mensagens, incluindo detalhes da comunicação
 
-	// $mail->setFrom('robertoenricon@gmail.com', 'Sistema do Beto'); //Remetente
-	$mail->setFrom('roberto.nicolo@integralmedica.com.br', 'Sistema do Beto'); //Remetente
+	// $mail->setFrom('roberto.nicolo@integralmedica.com.br', 'Sistema do Beto'); //Remetente
+	$mail->setFrom('robertoenrico_@hotmail.com', 'Sistema do Beto'); //Remetente
 
 	$mail->AddAddress($email); //Destinatarios
 
 	$mail->isHTML(true); //Define que o e-mail será enviado como HTML
 
-	$html = 'Corpo do email';
+	// if ($_SERVER['SCRIPT_FILENAME'] == "C:/xampp/htdocs/bla/email.php") {
+
+		$html = '
+			<html>
+			<head>
+			<title>Confirmação de Cadastro</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+			</head>
+		
+			<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+		
+			<table id="Tabela_01" width="600" height="922" border="0" cellpadding="0" cellspacing="0">
+				<tr>
+					<td>
+						<img src="https://hospedagemimagem.000webhostapp.com/img/header.png" width="600" height="115" alt="Baixar imagens" style="display:block;">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="https://hospedagemimagem.000webhostapp.com/img/section2.png" width="600" height="300" alt="Baixar imagens" style="display:block;">
+					</td>
+				</tr>
+				<tr>
+					<! -- <td width="600" height="320" style="font-size: 18px; background: linear-gradient(to right, #e3e3e3, white, white, white, white, white, #e3e3e3);"> -->
+					<td width="600" height="320" style="font-size: 18px; background-color: #e3e3e3;">
+						<br>
+						<p style="text-align: center;">Caro Lojista.</p>
+						<p style="margin-left:5%;">Recebemos seu contato pelo nosso site manifestando interesse em ter os <b>suplementos Darkness</b> em sua loja.<br>
+						<b>Você está no caminho certo! Afinal, é a linha mais hardcore do Brasil.</b><br>
+						Vamos dar andamento à sua solicitação, para isso basta<br>
+						seguir os próximos passos:
+						<li style="margin-left:5%;">Clique no link abaixo e confirme seu cadastro.</li>
+						<li style="margin-left:5%;">Aguarde o e-mail de efetivação.</li>
+						<li style="margin-left:5%;">Após a efetivação, você receberá o retorno de um representante em <br>até 5 dias úteis.</li>
+						</p>
+						<p style="text-align: center;">Até breve!</p>
+						<a href="#"><center><img src="https://hospedagemimagem.000webhostapp.com/img/btn.png" style="display:block;"></center></a><br>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="https://hospedagemimagem.000webhostapp.com/img/footer.png" width="600" alt="Baixar imagens" style="display:block;">
+					</td>
+				</tr>
+			</table>
+		
+			</body>
+			</html>
+		';
+	
+	// }
 
 	$mail->Subject = 'Titulo - ' . $email;
 	$mail->Body    = $html;
-	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 	if($mail->send()) {
-		print "<br>Enviado";
+		print "<br>Email Enviado";
 	} else {
 		print '<br>Erro ao enviar o e-mail: <br>';
 		print 'Mailer Error: ' . $mail->ErrorInfo;
